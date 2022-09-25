bc066f95184d8a58f77770304f81c5545e07a74f
Beshir Saeed <Beshir-saeed@live.com>
2022-09-25 12:02:23 +0300

    Add local passport strategy

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
+const validator = require("validator");
 
 //@desc Auth with Google
 //@route GET /auth/google
@@ -15,6 +16,50 @@ router.get(
 		res.redirect("/dashboard");
 	}
 );
+
+//@desc Redirect from login
+//@route GET /auth/login
+router.get("/login", (req, res) => {
+	if (req.user) {
+		return res.redirect("/dashboard");
+	}
+	res.render("/", {
+		title: "Login",
+	});
+});
+
+//@desc logging in
+//@route POST /auth/login
+router.post("/login", (req, res, next) => {
+	const validationErrors = [];
+	if (!validator.isEmail(req.body.email))
+		validationErrors.push({ msg: "Please enter a valid email address." });
+	if (validator.isEmpty(req.body.password))
+		validationErrors.push({ msg: "Password cannot be blank." });
+
+	if (validationErrors.length) {
+		req.flash("errors", validationErrors);
+		return res.redirect("../");
+	}
+	req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
+
+	passport.authenticate("local", (err, user, info) => {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			req.flash("errors", info);
+			return res.redirect("/");
+		}
+		req.logIn(user, (err) => {
+			if (err) {
+				return next(err);
+			}
+			req.flash("success", { msg: "Success! You are logged in." });
+			res.redirect(req.session.returnTo || "/dashboard");
+		});
+	})(req, res, next);
+});
 
 //@desc Logout user
 //@route /auth/logout
